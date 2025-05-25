298aaee088ed2dfc57e7afde0ba7b42385bd93d7
AxelDevMS <asmtel2001@gmail.com>
2025-05-25 14:05:43 -0600

    feat:create project

---
+++ src/main/java/com/ams/dev/api/parking/Application.java
@@ -0,0 +1,13 @@
+package com.ams.dev.api.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ams/dev/api/parking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.dev.api.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
